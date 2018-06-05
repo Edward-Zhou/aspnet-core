8556a7a1ed32b0c4b385b32e7a81449365dd1474
Edward <v-tazho@hotmail.com>
2018-06-05 05:28:07 -0400

    enum extension

---
+++ src/EdwardAbp.Core/Extensions/EdwardExtension.cs
@@ -0,0 +1,37 @@
+﻿using Abp.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace EdwardAbp.Extensions
+{
+    public static class EdwardExtension
+    {
+        public static T ToValue<T>(string displayName) 
+        {
+            var result = typeof(T).GetFields()
+                            .Where(m => m.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName == displayName)
+                            .FirstOrDefault()
+                            ?.GetValue(null);
+            if (result == null)
+            {
+                throw new UserFriendlyException($"There is no corresponding Enum Item with DisplayName {displayName}!");
+            }
+            return (T)result;
+        }
+        public static string ToDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetAttribute<DisplayNameAttribute>()?.DisplayName;
+        }
+        public static TAttribute GetAttribute<TAttribute>(this Enum enumValue) where TAttribute : Attribute
+        {
+            return enumValue.GetType()
+                            .GetMember(enumValue.ToString())
+                            .First()
+                            .GetCustomAttribute<TAttribute>();
+        }
+    }
+}
